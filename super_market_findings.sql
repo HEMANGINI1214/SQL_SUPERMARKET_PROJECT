64b62a8a767e946c1d9f65d29558e311d9b97fdf
HEMANGINI1214 <hemanginik1406@gmail.com>
2025-01-09 12:15:03 +0530

    Add files via upload

---
+++ super_market_findings.sql
@@ -0,0 +1,36 @@
+# Use Case 1 Findings
+# Chicago is the best performing store by total revenue and also has the highest average items per customer.
+# Despite being the only store in Branch B the Los Angeles location has around the same as both stores in Branch A in average items per customer.
+# This shows that if the company was thinking of expanding to two stores in Branch B than one can expect a slightly below average performing store.
+
+
+# Use Case 2 Findings
+# New York Location Customer Behavior KPIs Conclusions.
+# New York has the highest amount of customers out of all locations, but is behind Chicago in Membership Percentage and Big Spenders.
+# If I were to recommend improvements for New York, it would be to increase their Membership push, more Members = more returning customers.
+# Chicago Location Customer Behavior KPIs Conclusions.
+# Chicago rates the highest in Membership Percentage and Big Spenders, but is second behind New York in total customers.
+# If I were to recommend improvements for Chicago, it would be to increase the amount of female centric products.
+# The numbers show that only 44.2% of customers are female, so the store can gain more female customers with these products.
+# More female products = more total customers.
+# Los Angeles Location Customer Behavior KPIs Conclusions.
+# Los Angeles is the lowest performing store, so there is tons of room for improvement. 
+# Los Angeles biggest area of improvement is with the amount of Big Spenders. Despite only having 4 less total customers than Chicago.
+# Despite only having 4 less total customers than Chicago, Los Angeles has 24 less Big Spenders than Chicago.
+# To improve this Los Angeles could advertise more expensive items at the front of the store.
+# However I think the answer is to boost their Membership count. 
+# That way when Members need to buy one of these big ticket items, they would be more likely to choose this store so they can earn more rewards points. 
+
+# Use Case 3 Findings
+# Overall Company Product Performance KPIs
+# Use Case 3.1 shows that Shampoo is the companies highest performing product.
+# My recommendation is to capitalize off of that and also order conditioners, body wash, and possibly shower caps. 
+# Use Case 3.3 shows that Personal Care  is the highest performing product category.
+# My recommendation would be the same as my Use Case 3.1 recommendation.
+# It would be useful to pursue Personal Care products outside of bath wear, maybe pursue fragrances or dental products. 
+# Product Performances KPIs by Store Locations
+# Use Case 3.2 shows that Orange Juice is the top product in both the Chicago and Los Angeles locations.
+# My recommendation is to expanding the line of beverages the company has to offer. 
+# These 2 stores would be a great place to test the market when selling a new beverage.
+# Use Case 3.4 shows that Personal Care is the highest contributing category for the New York location.
+# This aligns with Use Case 3.3's findings. If the company pursues the recommendation of expanding their personal care selection, this would be a great location to test the market.
